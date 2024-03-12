8bd3992c98768a74f328bb89df5f80f25295b74c
anna-levina <anna_levina@internet.ru>
2024-03-12 18:40:19 +0300

    Перемещен config.py

--- tests/test_cost_table.py
+++ tests/test_cost_table.py
@@ -1,12 +1,10 @@
-import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
-from config import api_domain
+from tests.config import api_domain
 
 
 def test_filter_by_category():
@@ -14,9 +12,6 @@ def test_filter_by_category():
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--remote-debugging-port=9222")
 
     driver = webdriver.Chrome(options=chrome_options)
 
