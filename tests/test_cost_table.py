a382db16f107157adaab5735197cd5648fdedf67
anna-levina <anna_levina@internet.ru>
2024-03-12 16:57:39 +0300

    Добавлены аргументы для ChromeOptions

--- tests/test_cost_table.py
+++ tests/test_cost_table.py
@@ -11,8 +11,11 @@ import time
 def test_filter_by_category():
     # Настройка опций Chrome для запуска в headless режиме
     chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Запуск Chrome в фоновом режиме
-    chrome_options.add_argument("--disable-gpu")  # Отключение GPU
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--remote-debugging-port=9222")
 
     driver = webdriver.Chrome(options=chrome_options)
 
