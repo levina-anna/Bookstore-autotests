91631fea6c4f9c345d0ff8662e9f2295007dae88
levina-anna <anna_levina@internet.ru>
2024-03-18 10:32:25 +0300

    Добавлены аргументы

--- tests/test_cost_table.py
+++ tests/test_cost_table.py
@@ -11,6 +11,8 @@ def test_filter_by_category():
     # Настройка опций Chrome для запуска в headless режиме
     chrome_options = Options()
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
 
     driver = webdriver.Chrome(options=chrome_options)
