0bdace41f595b74155572f2750ac0726f9003448
levina-anna <anna_levina@internet.ru>
2024-04-12 20:27:32 +0300

    Раскомментирован запуск тестов в фоновом режиме

--- tests/test_cost_table.py
+++ tests/test_cost_table.py
@@ -10,7 +10,7 @@ from config import domain
 def test_filter_by_category():
     # Настройка опций Chrome
     chrome_options = Options()
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
